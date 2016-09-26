8d0abe3498f2177012b6b3fc9b62be8ea9079db4
onlyjus <onlyjus@gmail.com>
2016-09-25 22:07:38 -0400

    played with spacing and colors, added target temp line

--- pibrewery/pibrewery.py
+++ pibrewery/pibrewery.py
@@ -4,6 +4,14 @@
 # Licensed under the terms of the MIT License
 # see LICENSE
 
+import logging
+DATA_LOGGER = logging.getLogger('data')
+DATA_LOGGER.setLevel(logging.INFO)
+fh = logging.FileHandler('./data.log')
+fr = logging.Formatter('%(asctime)s %(message)s')
+fh.setFormatter(fr)
+DATA_LOGGER.addHandler(fh)
+
 import kivy
 kivy.require('1.9.1')
 
@@ -12,10 +20,11 @@ from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.garden.graph import Graph, MeshLinePlot
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, NumericProperty, StringProperty
 
 from read_sensors import ReadSensors
 from tools import c_to_f
+from constants import *
 
 class MainScreen(Screen):
     pass
@@ -30,7 +39,10 @@ class ScreenManagement(ScreenManager):
     pass
 
 class PiBrewery(App):
-    running_values = ListProperty([])
+    temperature_values = ListProperty([])
+    temperature_max = NumericProperty(100)
+    temperature_min = NumericProperty(50)
+    current_temperature = StringProperty('##')
     sensors = ReadSensors()
 
     def build(self):
@@ -41,8 +53,13 @@ class PiBrewery(App):
     def update_fermentation_plot(self, dt):
         temp = self.sensors.read_temperature()
         temp = c_to_f(temp)
-        self.running_values.append(temp)
-        self.running_values = self.running_values[-100:]
+        temp_str = '{0:.1f}'.format(temp)
+        DATA_LOGGER.info(temp_str)
+        self.current_temperature = temp_str
+        self.temperature_values.append(temp)
+        self.temperature_values = self.temperature_values[-MAXIMUMPLOTLENGTH:]
+        self.temperature_max = max(self.temperature_values) + 1
+        self.temperature_min = min(self.temperature_values) - 1
 
 if __name__ == '__main__':
     PiBrewery().run()
